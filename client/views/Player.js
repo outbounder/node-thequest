65d8e42d843a8653cd00a27580cf6c0da74a2c82
blez <snify@abv.bg>
2012-10-25 23:10:56 -0700

    New skins

--- client/views/Player.js
+++ client/views/Player.js
@@ -1,16 +1,18 @@
 module.exports = function(data){
   _.extend(this, data);
-  this.$el = $("<div></div>");
+  this.$el = $("<div><div class='name'></div><div class='coin'></div></div>");
 }
 
 _.extend(module.exports.prototype, {
   render: function(){
-    this.$el.html(this.username);
+    this.$el.find(".name").html(this.username);
     this.$el.addClass("player");
+    
     if(this.hasTreasure)
-      this.$el.addClass("hasTreasure");
+      this.$el.find(".coin").show();//addClass("hasTreasure");
     else
-      this.$el.removeClass("hasTreasure");
+      this.$el.find(".coin").hide();//this.$el.removeClass("hasTreasure");
+
     this.$el.css({
       left: this.x,
       top: this.y
